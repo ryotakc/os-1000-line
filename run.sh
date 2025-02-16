9b379e0ee37256f8ecf3bd74b1c4a14ace002c90
Ryota Kato <rka147@sfu.ca>
2025-02-16 01:50:31 -0800

    add kernel.c and fixed ld and sh

--- run.sh
+++ run.sh
@@ -4,5 +4,14 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+CC=clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Build Kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+  kernel.c
+
 # QEMU start command
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+  -kernel kernel.elf
